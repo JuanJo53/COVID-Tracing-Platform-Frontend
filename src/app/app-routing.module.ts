0ea7b587192e2494afd746875287cd2eca50f9c2
Juan Jose Fernandez <juanjofer1999@gmail.com>
2021-04-02 01:10:31 -0400

    some routes added

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,16 +1,39 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { MainComponent } from './layout/main/main.component';
+import { HomeComponent } from './modules/home/pages/home/home.component';
 
 const routes: Routes = [
   {
     path: '',
-    redirectTo: '/home',
+    redirectTo: '/inicio',
     pathMatch: 'full',
   },
   {
-    path: 'home',
+    path: '',
     component: MainComponent,
+    children: [
+      {
+        path: 'inicio',
+        component: HomeComponent,
+      },
+      {
+        path: 'noticias',
+        component: HomeComponent,
+      },
+      {
+        path: 'bolivia',
+        component: HomeComponent,
+      },
+      {
+        path: 'mundo',
+        component: HomeComponent,
+      },
+    ],
+  },
+  {
+    path: '**',
+    redirectTo: '/inicio',
   },
 ];
 
