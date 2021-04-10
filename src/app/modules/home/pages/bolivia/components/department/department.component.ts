9b4b8721eba5966be94db302af6cfd9b855b36fc
Juan Jose Fernandez <juanjofer1999@gmail.com>
2021-04-10 16:53:44 -0400

    dynamic table done

--- src/app/modules/home/pages/bolivia/components/department/department.component.ts
+++ src/app/modules/home/pages/bolivia/components/department/department.component.ts
@@ -8,7 +8,28 @@ import { Department } from 'src/app/shared/models/department';
 })
 export class DepartmentComponent implements OnInit {
   @Input() depto: Department;
-
+  deptos: Department[] = [
+    { name: 'Hydrogen', description: 'HOLA' },
+    { name: 'Helium', description: 'HOLo' },
+    { name: 'Lithium', description: 'HOLi' },
+    { name: 'Beryllium', description: 'HOLi' },
+    { name: 'Boron', description: 'HOLi' },
+    { name: 'Carbon', description: 'HOLi' },
+    { name: 'Nitrogen', description: 'HOLi' },
+    { name: 'Oxygen', description: 'HOLi' },
+    { name: 'Fluorine', description: 'HOLi' },
+    { name: 'Sodium', description: 'HOLi' },
+    { name: 'Magnesium', description: 'HOLi' },
+    { name: 'Aluminum', description: 'HOLi' },
+    { name: 'Silicon', description: 'HOLi' },
+    { name: 'Phosphoru', description: 'HOLi' },
+    { name: 'Sulfur', description: 'HOLi' },
+    { name: 'Chlorine', description: 'HOLi' },
+    { name: 'Argon', description: 'HOLi' },
+    { name: 'Potassium', description: 'HOLi' },
+    { name: 'Calcium', description: 'HOLi' },
+  ];
+  displayedColumns = ['id', 'name'];
   selected = 'general';
   constructor() {}
 
--- src/app/modules/home/pages/bolivia/components/table/table.component.ts
+++ src/app/modules/home/pages/bolivia/components/table/table.component.ts
@@ -1,4 +1,10 @@
-import { AfterViewInit, Component, OnInit, ViewChild } from '@angular/core';
+import {
+  AfterViewInit,
+  Component,
+  Input,
+  OnInit,
+  ViewChild,
+} from '@angular/core';
 import { MatPaginator } from '@angular/material/paginator';
 import { MatSort } from '@angular/material/sort';
 import { MatTable, MatTableDataSource } from '@angular/material/table';
@@ -10,27 +16,8 @@ import { Department } from 'src/app/shared/models/department';
   styleUrls: ['./table.component.scss'],
 })
 export class TableComponent implements OnInit, AfterViewInit {
-  deptos: Department[] = [
-    { name: 'Hydrogen', description: 'HOLA' },
-    { name: 'Helium', description: 'HOLo' },
-    { name: 'Lithium', description: 'HOLi' },
-    { name: 'Beryllium', description: 'HOLi' },
-    { name: 'Boron', description: 'HOLi' },
-    { name: 'Carbon', description: 'HOLi' },
-    { name: 'Nitrogen', description: 'HOLi' },
-    { name: 'Oxygen', description: 'HOLi' },
-    { name: 'Fluorine', description: 'HOLi' },
-    { name: 'Sodium', description: 'HOLi' },
-    { name: 'Magnesium', description: 'HOLi' },
-    { name: 'Aluminum', description: 'HOLi' },
-    { name: 'Silicon', description: 'HOLi' },
-    { name: 'Phosphoru', description: 'HOLi' },
-    { name: 'Sulfur', description: 'HOLi' },
-    { name: 'Chlorine', description: 'HOLi' },
-    { name: 'Argon', description: 'HOLi' },
-    { name: 'Potassium', description: 'HOLi' },
-    { name: 'Calcium', description: 'HOLi' },
-  ];
+  @Input() data: Department[];
+  @Input() displayedColumns: [];
 
   isLoadingResults = true;
   isRateLimitReached = false;
@@ -45,7 +32,6 @@ export class TableComponent implements OnInit, AfterViewInit {
   @ViewChild(MatPaginator, { static: true }) paginator: MatPaginator;
   @ViewChild(MatTable, { static: true }) table: MatTable<Department>;
 
-  displayedColumns = ['id', 'name'];
   // dataSource: Department;
 
   constructor() {}
@@ -53,18 +39,18 @@ export class TableComponent implements OnInit, AfterViewInit {
   ngOnInit(): void {
     // this.fectchData();
   }
-  ngAfterViewInit() {
-    this.dataSource = new MatTableDataSource(this.deptos);
+  ngAfterViewInit(): void {
+    this.dataSource = new MatTableDataSource(this.data);
     this.dataSource.sort = this.sort;
     this.dataSource.paginator = this.paginator;
     // this.table.dataSource = this.dataSource;
   }
-  fectchData() {
-    this.dataSource = new MatTableDataSource(this.deptos);
+  fectchData(): void {
+    this.dataSource = new MatTableDataSource(this.data);
     this.dataSource.sort = this.sort;
   }
 
-  applyFilter(event: Event) {
+  applyFilter(event: Event): void {
     const filterValue = (event.target as HTMLInputElement).value;
     this.dataSource.filter = filterValue.trim().toLowerCase();
   }
