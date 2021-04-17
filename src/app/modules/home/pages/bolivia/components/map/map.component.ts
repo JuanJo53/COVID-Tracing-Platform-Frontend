552b65676cfde7bc883f6cf77cc74ae5061ab897
Juan Jose Fernandez <juanjofer1999@gmail.com>
2021-04-17 16:23:00 -0400

    change circles done

--- src/app/modules/home/pages/bolivia/components/map/map.component.ts
+++ src/app/modules/home/pages/bolivia/components/map/map.component.ts
@@ -15,8 +15,15 @@ export class MapComponent implements OnInit {
 
   myMap: Leaflet.Map;
 
-  constructor(private municipalityService: MunicipalityService) {}
+  circleDisplayType = 'confirmed';
 
+  confirmed: any[];
+  deaths: any[];
+  recovered: any[];
+  vaccined: any[];
+
+  constructor(private municipalityService: MunicipalityService) {}
+  circles;
   ngOnInit(): void {
     console.log(this.depto);
 
@@ -25,38 +32,115 @@ export class MapComponent implements OnInit {
     this.getMunicipalities();
   }
   getMunicipalities() {
+    this.confirmed = [];
     this.municipalityService
       .getAllDepartmentMunicipalities(this.depto.iso)
       .subscribe((munis) => {
         console.log(munis);
         this.municipalities = munis;
         this.municipalities.forEach((municip) => {
+          this.confirmed.push(
+            this.addCircles(
+              municip.latitude,
+              municip.longitude,
+              municip.confirmed,
+              '#999999'
+            )
+          );
+          this.circles = Leaflet.layerGroup(this.confirmed);
+          this.myMap.addLayer(this.circles);
+        });
+      });
+  }
+  changeData() {
+    this.confirmed = [];
+    this.deaths = [];
+    this.recovered = [];
+    this.vaccined = [];
+
+    this.myMap.removeLayer(this.circles);
+    if (this.circleDisplayType == 'confirmed') {
+      console.log(this.circleDisplayType);
+      this.municipalities.forEach((municip) => {
+        this.confirmed.push(
           this.addCircles(
             municip.latitude,
             municip.longitude,
-            municip.confirmed
-          );
-        });
+            municip.confirmed,
+            '#999999'
+          )
+        );
+        this.circles = Leaflet.layerGroup(this.confirmed);
+        this.myMap.addLayer(this.circles);
       });
+    } else if (this.circleDisplayType == 'deaths') {
+      this.municipalities.forEach((municip) => {
+        this.deaths.push(
+          this.addCircles(
+            municip.latitude,
+            municip.longitude,
+            municip.deaths,
+            'red'
+          )
+        );
+        this.circles = Leaflet.layerGroup(this.deaths);
+        this.myMap.addLayer(this.circles);
+      });
+    } else if (this.circleDisplayType == 'recovered') {
+      this.municipalities.forEach((municip) => {
+        this.recovered.push(
+          this.addCircles(
+            municip.latitude,
+            municip.longitude,
+            municip.recovered,
+            'green'
+          )
+        );
+        this.circles = Leaflet.layerGroup(this.recovered);
+        this.myMap.addLayer(this.circles);
+      });
+    } else if (this.circleDisplayType == 'vaccined') {
+      this.municipalities.forEach((municip) => {
+        this.vaccined.push(
+          this.addCircles(
+            municip.latitude,
+            municip.longitude,
+            municip.vacined,
+            'blue'
+          )
+        );
+        this.circles = Leaflet.layerGroup(this.vaccined);
+        this.myMap.addLayer(this.circles);
+      });
+    }
   }
   setMapLoc(lng, lat, zoom) {
     const location = {
       coords: new Leaflet.LatLng(lat, lng),
       zoom: zoom,
+      layers: [this.confirmed, this.deaths, this.recovered, this.vaccined],
     };
     Leaflet.tileLayer(
       'https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png'
     ).addTo(this.myMap);
     this.myMap.setView(location.coords, location.zoom);
   }
-  addCircles(lat: number, lng: number, volume: number): void {
+  addCircles(lat: number, lng: number, volume: number, color: string): any {
     var circle = Leaflet.circle([lat, lng], {
-      color: 'red',
-      fillColor: '#f03',
+      color: color,
+      fillColor: color,
       fillOpacity: 0.5,
       radius: volume * 5,
-    }).addTo(this.myMap);
+    });
     var detail = document.getElementById('detail');
     circle.bindPopup(detail);
+    return circle;
+  }
+  removeCircles(lat: number, lng: number, volume: number, color: string): any {
+    var circle = Leaflet.circle([lat, lng], {
+      fillOpacity: 0.5,
+      radius: volume * 5,
+    }).remove();
+    return circle;
   }
 }
