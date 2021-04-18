34d7a590232f770f57b75c59204299b7226012aa
XimeCruz <xmenacruzt@gmail.com>
2021-04-17 20:58:05 -0400

    Estructura de recomendaciones

--- src/app/modules/home/pages/recommendations/recommendations.component.ts
+++ src/app/modules/home/pages/recommendations/recommendations.component.ts
@@ -13,3 +13,19 @@ export class RecommendationsComponent implements OnInit {
   }
 
 }
+/*
+$('ul.tabs li a:first').addClass('active');
+	$('.secciones article').hide();
+	$('.secciones article:first').show();
+
+	$('ul.tabs li a').click(function(){
+		$('ul.tabs li a').removeClass('active');
+		$(this).addClass('active');
+		$('.secciones article').hide();
+
+		var activeTab = $(this).attr('href');
+		$(activeTab).show();
+		return false;
+	});
+
+*/
